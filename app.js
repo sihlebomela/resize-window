561d29da2eeb9b3f81762221347b5a292922f1e3
trinity-senpai <sihletrinity70@gmail.com>
2021-08-05 13:20:17 +0200

    mouse/ typing feature

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ let log = document.querySelector('.log');
 windowElem = document.querySelector('.window');
 
 windowElem.addEventListener('mousemove', (mouse) => {
-    if(mouse.x > windowElem.offsetWidth/1.2) {
+    if(mouse.x > windowElem.offsetWidth) {
         windowElem.addEventListener('mousemove', (mouse) => {
             if (mouse.which == 1 && mouse.x > (windowElem.offsetWidth/windowElem.offsetWidth)) {  // right mouse btn is clicked
                document.querySelector('.window').style.width = `${mouse.x + 10}px`;
@@ -18,3 +18,22 @@ resizeBtn.addEventListener('mouseup', () => {
     resizeBtn.classList.remove('grabbed')
 })
 
+let text = `> terminal running
+> installing html...
+> html installed
+> GET [https://nasa.org]
+> hacking NASA with html...
+> ... ... ... ...
+> No secrets found`;
+let editor = document.querySelector('code');
+
+let i = 0;
+let speed = Math.ceil(Math.random() * 100);
+
+setInterval(() => {
+    if (i < text.length) {
+        speed = Math.ceil(Math.random() * 1000);
+        editor.innerText += text[i]
+        i+=1;
+    }
+}, speed);
